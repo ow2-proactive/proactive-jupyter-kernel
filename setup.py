9641350ecb8e7efc290bbbae6ac723a5bf5e9552
Andrews Sobral <andrewssobral@gmail.com>
2019-06-12 22:21:55 +0200

    Fix for AttributeError: module 'tornado.web' has no attribute 'asynchronous'

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ package_data = \
 
 install_requires = \
     ['pexpect>=4.0', 'proactive', 'jupyter_client', 'IPython', 'traitlets', 'ipykernel', 'notebook', 'configparser',
-     'matplotlib', 'networkx', 'pygraphviz']
+     'matplotlib', 'networkx', 'pygraphviz', 'tornado==5.1.1']
 
 now = datetime.datetime.now()
 
