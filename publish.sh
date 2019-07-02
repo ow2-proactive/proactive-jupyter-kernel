b6eece94090283031b2fad2ce97694bd5bfa34bf
mklkun <mohamed.khalil.labidi@gmail.com>
2019-07-02 16:16:38 +0200

    jenkins fix in publish.sh file

--- publish.sh
+++ publish.sh
@@ -1,2 +1,9 @@
 #!/usr/bin/env bash
-twine upload dist/* --config-file .pypirc
+#twine upload dist/* --config-file .pypirc
+
+if [[ $JENKINS_JNLP_URL ]]
+then
+   twine upload -r pypi dist/* --config-file /home/activeeon/.pypirc
+else
+   twine upload -r pypi dist/*
+fi
