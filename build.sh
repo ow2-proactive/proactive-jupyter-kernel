01c65821698681cce06b9e53c4302175d208b711
Andrews Sobral <andrewssobral@users.noreply.github.com>
2019-08-06 17:08:25 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,21 @@
-#!/usr/bin/env bash
+#!/bin/bash
+command -v source >/dev/null 2>&1 || {
+  echo "I require source but it's not installed.  Aborting." >&2; exit 1;
+}
+
+#pip list
+
+pip install virtualenv
+#which virtualenv
+
+virtualenv -p python3 env
+#virtualenv -p python2 env
+source env/bin/activate
+
+#pip install -U pip
+
 # https://github.com/pypa/warehouse/issues/5890#issuecomment-494868157
-pip install -U pip
-pip install -U twine==1.13.0 wheel setuptools
+pip install -U twine wheel setuptools
 
 rm -rf dist/
 rm -rf build/
